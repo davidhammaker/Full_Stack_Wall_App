566d31580770d4336f03d9668cb87dd07ddeff71
David J. Hammaker <davidjosh93@gmail.com>
2019-08-09 20:06:08 -0400

    Create 'Post' model
    Add new model class for wall posts.

--- backend/wallproject/wall/models.py
+++ backend/wallproject/wall/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.creator} {self.date_posted}'
