3849079060e23c7a3ec09296f855c2dd2c8ee850
David J. Hammaker <davidjosh93@gmail.com>
2019-08-09 23:09:44 -0400

    Override 'perform_create' in PostList view
    Override 'perform_create' to ensure that the creator for each new post 
    is set to the current user.

--- backend/wallproject/wall/views.py
+++ backend/wallproject/wall/views.py
@@ -7,6 +7,12 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        """
+        Attach users to posts by overriding `perform_create`.
+        """
+        serializer.save(creator=self.request.user)
+
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
