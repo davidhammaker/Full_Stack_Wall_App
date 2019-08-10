5e4ee98d65a28456d29b40ec89c53e9c8e4a40c7
David J. Hammaker <davidjosh93@gmail.com>
2019-08-10 12:09:32 -0400

    Add frontend/backend interaction
    Use Axios to render backend data on the frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,59 @@
 import React from 'react';
+import axios from 'axios';
+
+
+function Post(props) {
+  const post = props.post;
+  return (
+    <div className="p-2">
+      <h5 className="h-100">{ post.creator }
+        <small className="float-right font-italic small-font">
+          { post.date_posted }
+        </small>
+      </h5>
+      <p className="pl-2 large-font">{ post.content }</p>
+    </div>
+  );
+}
+
+
+class ListedPosts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {posts: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000')
+    .then(response => {
+      const data = response.data;
+      this.setState({posts: data});
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+      { 
+        this.state.posts.map((post) => 
+        <Post key={ post.id } post={ post } />
+        ) 
+      }
+      </div>
+    )
+  }
+}
+
 
 function App() {
   return (
-    <div>placeholder</div>
+    <div className="col-sm-8 mx-auto text-light bg-secondary">
+      <h1 className="p-4">Placeholder</h1>
+      <ListedPosts />
+    </div>
   );
 }
 
