e9b2017be8c011f4539318ce5f47936cdd5069fc
David J. Hammaker <davidjosh93@gmail.com>
2019-08-10 18:50:57 -0400

    Paginate backend posts by 10 per page

--- backend/wallproject/wallproject/settings.py
+++ backend/wallproject/wallproject/settings.py
@@ -139,3 +139,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
+# Pagination
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
