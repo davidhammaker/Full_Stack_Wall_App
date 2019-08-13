839d306545895e435f4b3cefa0742329bcd76c69
David J. Hammaker <davidjosh93@gmail.com>
2019-08-12 20:15:31 -0400

    Add a few stylistic changes to the frontend

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -114,7 +114,7 @@ function SignUp(props) {
   return (
     <div>
       <a className="btn btn-dark float-right mt-4 mr-4" href="/">Home</a>
-      <h1 className="p-4">Sign Up</h1>
+      <h1 className="p-4 border border-left-0 border-top-0 border-right-0">Sign Up</h1>
       <SignUpForm />
     </div>
   )
--- frontend/src/components/wall.js
+++ frontend/src/components/wall.js
@@ -73,7 +73,10 @@ function Wall(props) {
   return (
     <div>
       <a className="btn btn-dark float-right mt-4 mr-4" href="/signup">Sign Up</a>
-      <h1 className="p-4">Placeholder</h1>
+      <h1
+        className="display-4 p-4 border border-left-0 border-top-0 border-right-0">
+          Wall App
+      </h1>
       <ListedPosts />
     </div>
   )
