f0a32b288b6a967abd8676b53076a40f836c303d
David J. Hammaker <davidjosh93@gmail.com>
2019-08-13 17:52:48 -0400

    Extract 'getCookie' function into separate file
    Move the 'getCookie' function from 'signup.js' to a new file in a 
    separate directory, 'utils/cookiefunctions.js'. Import 'getCookie' from 
    'cookiefunctions.js'.

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import { getCookie } from './utils/cookiefunctions';
 
 
-function getCookie(name) {
-  var cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-          var cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
-      }
-  }
-  return cookieValue;
-}
 var csrftoken = getCookie('csrftoken');
 
 
