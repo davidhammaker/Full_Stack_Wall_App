e7bed843adb8f1999713e9bb9d3c03e430b77f9a
David J. Hammaker <davidjosh93@gmail.com>
2019-08-13 20:32:26 -0400

    Remove 'Link' import
    Remove unused import from 'App.js'.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Wall from './components/wall';
 import SignUp from './components/signup';
 import Login from './components/login';
