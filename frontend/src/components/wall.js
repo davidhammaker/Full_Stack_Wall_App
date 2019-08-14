83eb9ac3113f6f322ff411140f20528aa1627f59
David J. Hammaker <davidjosh93@gmail.com>
2019-08-13 22:44:42 -0400

    Display a message if there are no posts

--- frontend/src/components/wall.js
+++ frontend/src/components/wall.js
@@ -43,14 +43,21 @@ function Post(props) {
 
 
 function ListedPosts(props) {
+  if (props.posts.toString() !== '') {
+    return (
+      <>
+      { 
+        props.posts.map((post) => 
+        <Post key={ post.id } post={ post } />
+        ) 
+      }
+      </>
+    )
+  }
   return (
-    <>
-    { 
-      props.posts.map((post) => 
-      <Post key={ post.id } post={ post } />
-      ) 
-    }
-    </>
+    <div className="font-italic pb-4 mb-2 d-flex justify-content-center">
+      Nothing has been posted yet.
+    </div>
   )
 }
 
