9130dba6b25be6b43515f71938a5870b1cfb48ba
David J. Hammaker <davidjosh93@gmail.com>
2019-08-14 21:52:18 -0400

    Create more frontend tests
    Add several tests to the frontend, all contained within 'App.test.js'.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 import App from './App';
 import Post from './components/post';
+import ListedPosts from './components/listedposts';
+import Links from './components/links';
+import Welcome from './components/welcome';
+import PostForm from './components/postform';
+import Form from './components/form';
 
 
 configure({ adapter: new Adapter() });
@@ -18,7 +24,6 @@ it('renders without crashing', () => {
 
 // Posts
 it('displays a post', () => {
-  // Render a post with content
   const testPost = {
     id: 1,
     content: 'test 1',
@@ -33,7 +38,6 @@ it('displays a post', () => {
 });
 
 it('displays a re-formatted date', () => {
-  // Render a post with content
   const testPost = {
     id: 1,
     content: 'test 1',
@@ -47,18 +51,124 @@ it('displays a re-formatted date', () => {
   expect(post.text()).toContain('12:17 AM, 08/14/2019');
 });
 
-it('displays the correct content', () => {
-  // Render a post with content
+it('displays post correctly', () => {
   const testPost = {
     id: 1,
     content: 'test 1',
     username: 'testUser',
     date_posted: '2019-08-14T00:17:28.427013Z'
   };
-  const post = shallow(
+  const post = renderer.create(
     <Post key={ testPost.id } post={ testPost } />
   );
+  let tree = post.toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  expect(post.text()).toContain('test 1');
+
+// ListedPosts
+it('displays the correct number of posts', () => {
+  // Render a posts with content
+  const testPosts = [
+    {
+      id: 2,
+      content: 'test 2',
+      username: 'testUser',
+      date_posted: '2019-08-14T00:17:28.427013Z'
+    },
+    {
+      id: 1,
+      content: 'test 1',
+      username: 'testUser',
+      date_posted: '2019-08-13T00:17:28.427013Z'
+    }
+  ];
+  const listedPosts = shallow(
+    <ListedPosts posts={ testPosts } />
+  );
+
+  expect(listedPosts.contains(
+    <>
+      <Post key="2" post={ testPosts[0] } />
+      <Post key="1" post={ testPosts[1] } />
+    </>
+    )
+  ).toBe(true);
+});
+
+it('displays message if no posts', () => {
+  // Render a posts with content
+  const testPosts = [];
+  const listedPosts = shallow(
+    <ListedPosts posts={ testPosts } />
+  );
+
+  expect(listedPosts.text()).toContain('Nothing has been posted yet.');
+});
+
+
+// Links
+it('displays login/signup links if not logged in', () => {
+  // Render links
+  const links = shallow(
+    <Links />
+  );
+
+  expect(links.text()).toContain('Log In');
+  expect(links.text()).toContain('Sign Up');
+});
+
+it('displays logout link if logged in', () => {
+  // Render links with token
+  const links = shallow(
+    <Links token='token' />
+  );
+
+  expect(links.text()).toContain('Log Out');
+});
+
+
+// Welcome
+it('displays login request if not logged in', () => {
+  // Render welcome
+  const welcome = shallow(
+    <Welcome />
+  );
+
+  expect(welcome.text()).toContain('Please log in to post on the wall.');
+});
+
+it('displays welcome if logged in', () => {
+  // Render welcome with token and username
+  const welcome = shallow(
+    <Welcome token='token' username='username' />
+  );
+
+  expect(welcome.text()).toContain('Welcome, username!');
+});
+
+
+// PostForm
+it('displays post form correctly', () => {
+  const postForm = renderer.create(
+    <PostForm username='username' token='token' />
+  );
+  let tree = postForm.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+// Form
+it('displays nothing when not logged in', () => {
+  const form = shallow(<Form />);
+  expect(form.contains(<></>)).toBe(true);
+})
+
+it('displays form when logged in', () => {
+  const form = renderer.create(
+    <Form username='username' token='token' />
+  );
+  let tree = form.toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
